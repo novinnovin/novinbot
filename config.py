02c3ee6d8041e63de1856f7484480791fc7d3369
novinnovin <yaphetsgotir@gmail.com>
2025-02-09 13:57:20 +0000

    init

---
+++ config.py
@@ -0,0 +1,10 @@
+admins = [5310603408, 823145861]  # admins user ids
+token = "6409348451:AAFD5CH2KFimFgwE3VTNd5bPfA-xaVDWxAo"
+
+host_db = "localhost"
+database = "Novinverifytelbot"  # database name
+user_db = "root"  # database username
+passwd_db = "46UsATNdaFgznMh"  # database password
+port = 3306
+
+python_version = "python3"
